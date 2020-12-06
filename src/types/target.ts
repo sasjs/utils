ad49ee7bd105da710dff45513874a5e6ed828be1
Krishna Acondy <kc.acondy@outlook.com>
2020-12-06 16:50:01 +0000

    fix(target): remodel target

--- src/types/target.ts
+++ src/types/target.ts
@@ -1,25 +1,46 @@
 import { ServerType } from './serverType'
 
+export interface Config {
+  macroVars: { [key: string]: string }
+  initProgram: string
+  termProgram: string
+}
+
+export interface BuildConfig extends Config {
+  buildOutputFileName: string
+}
+
+export interface DeployConfig {
+  deployServicePack: boolean
+  deployScripts: string[]
+}
+
+export interface ServiceConfig extends Config {
+  serviceFolders: string[]
+}
+
+export interface JobConfig extends Config {
+  jobFolders: string[]
+}
+
+export interface StreamConfig {
+  assetPaths: string[]
+  streamWeb: boolean
+  streamWebFolder: string
+  webSourcePath: string
+}
+
 export interface Target {
   name: string
   serverUrl: string
   serverType: ServerType
   appLoc: string
-  buildOutputFileName: string
-  deployServicePack: boolean
-  tgtBuildInit: string
-  tgtBuildTerm: string
-  tgtBuildVars: { [key: string]: string }
-  tgtDeployVars: { [key: string]: string }
-  tgtServiceVars: { [key: string]: string }
-  tgtDeployScripts: string[]
-  tgtMacros: string[]
-  tgtServices: string[]
-  tgtServiceInit: string
-  jobs: string[]
-  assetPaths: string[]
-  streamWeb: boolean
-  streamWebFolder: string
-  webSourcePath: string
+  buildConfig: BuildConfig
+  deployConfig: DeployConfig
+  //   deployVars: { [key: string]: string }
+  serviceConfig: ServiceConfig
+  jobConfig: JobConfig
+  streamConfig: StreamConfig
+  macroFolders: string[]
   programFolders: string[]
 }
