decb3e6bb2ee1610876313c864d4b2926956c87d
Yury Shkoda <shkoda.yuri@gmail.com>
2020-12-22 10:09:09 +0300

    fix(timestamp): fixed month

--- src/time/timestamp.ts
+++ src/time/timestamp.ts
@@ -2,7 +2,7 @@ export const timestampToYYYYMMDDHHMMSS = (timestamp: number = Date.now()) => {
   const date = new Date(timestamp)
 
   return (
-    [date.getFullYear(), date.getMonth(), date.getDay()].join('/') +
+    [date.getFullYear(), date.getMonth() + 1, date.getDay()].join('/') +
     ' ' +
     [date.getHours(), date.getMinutes(), date.getSeconds()].join(':')
   )
