aa899a52a41d882cd967ebff6d2046e78983d506
Yury Shkoda <shkoda.yuri@gmail.com>
2020-12-22 09:49:07 +0300

    test(timestamp): improved unit tests

--- src/time/timestamp.spec.ts
+++ src/time/timestamp.spec.ts
@@ -4,26 +4,16 @@ describe('timestampToYYYYMMDDHHMMSS', () => {
   const regExp = /^\d{4}\/\d{1,2}\/\d{1,2} \d{1,2}:\d{1,2}:\d{1,2}/
 
   it('should format provided timestamp', () => {
-    let timestamp = 0
-
-    expect(timestampToYYYYMMDDHHMMSS(timestamp)).toEqual('1970/0/4 3:0:0')
-
-    timestamp = 1608617468867
+    const timestamp = 1608617468867
 
     expect(timestampToYYYYMMDDHHMMSS(timestamp).match(regExp)).toBeTruthy()
-
-    const timestampMatchRegExp = timestampToYYYYMMDDHHMMSS(timestamp).match(
-      regExp
+    expect(timestampToYYYYMMDDHHMMSS(timestamp).match(regExp)!.length).toEqual(
+      1
     )
-
-    expect(timestampMatchRegExp!.length).toEqual(1)
   })
 
   it('should return formatted current time if timestamp was not provided', () => {
     expect(timestampToYYYYMMDDHHMMSS().match(regExp)).toBeTruthy()
-
-    const timestampMatchRegExp = timestampToYYYYMMDDHHMMSS().match(regExp)
-
-    expect(timestampMatchRegExp!.length).toEqual(1)
+    expect(timestampToYYYYMMDDHHMMSS().match(regExp)!.length).toEqual(1)
   })
 })
