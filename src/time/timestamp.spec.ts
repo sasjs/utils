45ab78a04f69db1a3e22e13e704d5430a0bd41e8
Yury Shkoda <shkoda.yuri@gmail.com>
2020-12-22 09:46:11 +0300

    test(timestamp): improved unit tests

--- src/time/timestamp.spec.ts
+++ src/time/timestamp.spec.ts
@@ -1,15 +1,25 @@
 import { timestampToYYYYMMDDHHMMSS } from './timestamp'
 
 describe('timestampToYYYYMMDDHHMMSS', () => {
-  it('should format provided timestamp', () => {
-    const timestamp = 1608617468867
+  const regExp = /^\d{4}\/\d{1,2}\/\d{1,2} \d{1,2}:\d{1,2}:\d{1,2}/
 
-    expect(timestampToYYYYMMDDHHMMSS(timestamp)).toEqual('2020/11/2 9:11:8')
+  it('should format provided timestamp', () => {
+    let timestamp = 0
+
+    expect(timestampToYYYYMMDDHHMMSS(timestamp)).toEqual('1970/0/4 3:0:0')
+
+    timestamp = 1608617468867
+
+    expect(timestampToYYYYMMDDHHMMSS(timestamp).match(regExp)).toBeTruthy()
+
+    const timestampMatchRegExp = timestampToYYYYMMDDHHMMSS(timestamp).match(
+      regExp
+    )
+
+    expect(timestampMatchRegExp!.length).toEqual(1)
   })
 
   it('should return formatted current time if timestamp was not provided', () => {
-    const regExp = /^\d{4}\/\d{1,2}\/\d{1,2} \d{1,2}:\d{1,2}:\d{1,2}/
-
     expect(timestampToYYYYMMDDHHMMSS().match(regExp)).toBeTruthy()
 
     const timestampMatchRegExp = timestampToYYYYMMDDHHMMSS().match(regExp)
