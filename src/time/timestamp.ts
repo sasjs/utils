6a30e9e38a2557dd80a69052cfbf6e3823ef3044
Yury Shkoda <shkoda.yuri@gmail.com>
2021-06-03 13:49:11 +0300

    feat(time): added 'generateTimestamp' utility

--- src/time/timestamp.ts
+++ src/time/timestamp.ts
@@ -17,3 +17,25 @@ export const timestampToYYYYMMDDHHMMSS = (timestamp: number = Date.now()) => {
     ].join(':')
   )
 }
+
+export function generateTimestamp(sep = '', sepIndex?: number): string {
+  const date = new Date()
+
+  let timestamp: string | (number | string)[] = [
+    date.getUTCFullYear(),
+    date.getUTCMonth() + 1,
+    date.getUTCDate(),
+    date.getUTCHours(),
+    date.getUTCMinutes(),
+    date.getUTCSeconds()
+  ]
+
+  timestamp = timestamp.map((item) => padWithNumber(item as number))
+
+  if (sepIndex && sep && sepIndex < timestamp.length && sepIndex >= 0)
+    timestamp.splice(sepIndex, 0, sep)
+
+  timestamp = timestamp.join(sepIndex ? '' : sep)
+
+  return timestamp
+}
