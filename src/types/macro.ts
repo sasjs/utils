f1b69c70e59fd06e95e2809e89132abd13f337fd
Yury Shkoda <shkoda.yuri@gmail.com>
2021-06-08 12:31:18 +0300

    feat(macro): added isMacroVars type guard

--- src/types/macro.ts
+++ src/types/macro.ts
@@ -5,3 +5,6 @@ export interface MacroVar {
 export interface MacroVars {
   macroVars: MacroVar
 }
+
+export const isMacroVars = (arg: any): arg is MacroVars =>
+  arg && typeof arg.macroVars === 'object' && !Array.isArray(arg.macroVars)
