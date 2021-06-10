7f2816c1d2035568d3e4ea9fbd44029e1a50b159
Mihajlo <mihajlo.medjedovic@outlook.com>
2021-06-10 14:45:18 +0200

    fix: comment updated

--- src/types/extraResponseAttributes.ts
+++ src/types/extraResponseAttributes.ts
@@ -1,6 +1,10 @@
 /**
  * Represents a SASjs request parameter that will select which attributes are wanted in
  * response object on top of default `webout` object.
+ * 
  * Full functionality is not built yet, currently only supported attribute is `log`
+ * 
+ * Default webout object will be standalone if none attributes selected.
+ * If at least one attribute is selected, webout object will be put inside `result` object.
  */
 export type ExtraResponseAttributes = 'file' | 'data' | 'log'
