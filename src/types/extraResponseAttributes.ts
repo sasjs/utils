7ad729b27056a5f14bcfc3a98296cbc29ecb8673
Mihajlo <mihajlo.medjedovic@outlook.com>
2021-06-10 14:48:39 +0200

    fix: attirbutes comment updated

--- src/types/extraResponseAttributes.ts
+++ src/types/extraResponseAttributes.ts
@@ -4,7 +4,7 @@
  * 
  * Full functionality is not built yet, currently only supported attribute is `log`
  * 
- * Default webout object will be standalone if none attributes selected.
- * If at least one attribute is selected, webout object will be put inside `result` object.
+ * By default the webout object returned from SAS will be returned directly (eg res.mydata)
+ * Otherwise if at least one attribute is selected, the webout object will be nested under an object named 'result' (eg res.result.mydata)
  */
 export type ExtraResponseAttributes = 'file' | 'data' | 'log'
