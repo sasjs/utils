e0323d0d53d78a43446c25fbcab30dd0cd93c293
sabhas <sabirbk06@gmail.com>
2021-07-01 22:55:47 +0500

    feat: if decoded string contains PREFIX then return decoded string minus PREFIX else return string as it is

--- src/utils/base64.ts
+++ src/utils/base64.ts
@@ -2,5 +2,10 @@ const PREFIX =
   '2prwuP9bRG6dEJjJpn2zkqNN79GqCX1zUAKVc1JEat1YRXPsG7GF2T3R5Uo2EdiQm9uXv0zy0byYejxD0mRmB6sXfScwPJRA2BVCsaZwQxWFNSHeUMVkakSnAZhPx2Y730Bb4TH5SaCGWs2483G3UPtVLX2CoUZL4dbmA1UfSmUaX3suGGeZz18QFB66ke21PoNI0Cwa'
 export const encodeToBase64 = (str: string) =>
   Buffer.from(PREFIX + str).toString('base64')
-export const decodeFromBase64 = (b64Encoded: string) =>
-  Buffer.from(b64Encoded, 'base64').toString()
+export const decodeFromBase64 = (str: string) => {
+  const decodedPassword = Buffer.from(str, 'base64').toString()
+  if (decodedPassword.startsWith(PREFIX)) {
+    return decodedPassword.split(PREFIX).pop()
+  }
+  return str
+}
