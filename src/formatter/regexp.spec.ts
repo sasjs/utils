f5a60fbe030ff0eeb0618a252613f428823ddfec
Yury Shkoda <shkoda.yuri@gmail.com>
2022-03-15 11:46:39 +0300

    feat(regexp): add bewLine utility

---
+++ src/formatter/regexp.spec.ts
@@ -0,0 +1,20 @@
+import { newLine } from './'
+import { isWindows } from '../utils'
+
+describe('newLine', () => {
+  it('should return correct new line regexp entry for windows', () => {
+    Object.defineProperty(process, 'platform', {
+      value: 'win32'
+    })
+
+    expect(newLine()).toEqual('\\r\\n')
+  })
+
+  it('should return correct new line regexp entry for darwin', () => {
+    Object.defineProperty(process, 'platform', {
+      value: 'darwin'
+    })
+
+    expect(newLine()).toEqual('\\n')
+  })
+})
